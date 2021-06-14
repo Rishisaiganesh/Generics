f0c70e193603242719e82005f3d0b61c6122b43a
Rishisaiganesh <crazyganesh67@gmail.com>
2021-06-14 20:28:30 +0530

    [Rishi]Adding:maximumNumbers

---
+++ Generics/Maximum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+   public class Maximum
+    {
+        public int FindIntMax(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            if (firstNumber > secondNumber && firstNumber > thirdNumber)
+            {
+                return firstNumber;
+            }
+            if (secondNumber > firstNumber && secondNumber > thirdNumber)
+            {
+                return secondNumber;
+            }
+            else
+                return thirdNumber;
+        }
+    }
+}
